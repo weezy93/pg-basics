30859385fcbf634a79dca101c9d6bf7042d231ff
Louisa Reese <louisa.e.reese@gmail.com>
2016-02-25 11:12:22 -0700

    completed first section of queries

--- singleClient.js
+++ singleClient.js
@@ -1,2 +1,37 @@
+var pg = require("pg");
+var connectionString = 'postgres://localhost:5432/pgintro';
+var client = new pg.Client(connectionString);
+client.connect();
+
+// client.query("INSERT INTO cities (name, country, rating) VALUES ('New York City', 'United States', 4);", function(err, result){
+// 		client.end();
+// });
 
 
+// client.query("SELECT * FROM cities WHERE id = 3;", function(err, result){
+// 	if(!err){
+// 		console.log("single city: ", result.rows);
+// 	} else {
+// 		console.log(err);
+// 	}
+// 	client.end();
+// });
+
+// client.query("UPDATE cities SET rating = 9 WHERE name= 'Chicago';", function(err, result){
+// 	if(!err){
+// 		console.log('updating');
+// 	} else {
+// 		console.log(err);
+// 	}
+// 	client.end();
+// });
+
+client.query("DELETE FROM cities WHERE name = 'New York City';", function(err, result){
+	console.log('deleting');
+	client.end();
+});
+
+client.query("SELECT * FROM cities", function(err, result){
+	console.log("table: ", result.rows);
+	client.end();
+});
